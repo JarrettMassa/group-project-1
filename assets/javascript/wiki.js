bbb787400fa17a8e9e0859d54f4fc1dfb1bd465b
Jarrett Massa <jarrettmassa@gmail.com>
2018-07-12 18:26:09 -0400

    LastFM

--- assets/javascript/wiki.js
+++ assets/javascript/wiki.js
@@ -3,8 +3,19 @@ $(document).ready(function() {
   $(".search-button").click(function(){
 
     var searchTerm = $(".input-text").val();
+
+    var toTitleCase = function (searchTerm) {
+      searchTerm = searchTerm.toLowerCase().split(' ');
+      for (var i = 0; i < searchTerm.length; i++) {
+          searchTerm[i] = searchTerm[i].charAt(0).toUpperCase() + searchTerm[i].slice(1);
+      }
+      return searchTerm.join(' ');
+  };
+
+  var searchTerm = toTitleCase(searchTerm);
+  // searchTerm = searchTerm.replace(" ", "+");
     
-    var queryURL = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&origin=*&exintro=&titles=" + searchTerm;
+    var queryURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&origin=*&exintro=&titles=" + searchTerm;
 
     $.ajax({
       url: queryURL,
